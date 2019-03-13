62ee9d6cbc3159d8b6bd35dd0f0313b5f873868e
fabianjacott <hellworld@fabianjacott.com>
2019-03-13 15:28:12 -0300

    added create/update/delete to the frond-end

--- blog/settings.py
+++ blog/settings.py
@@ -40,7 +40,10 @@ INSTALLED_APPS = [
     'post',
     'marketing',
     'tinymce',
+    'crispy_forms'
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
